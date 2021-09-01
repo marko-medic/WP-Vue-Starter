2519b788de19abbcd01b869baf64f64d8e3231b2
Markorf <marko.medic59@gmail.com>
2021-09-01 14:03:10 +0200

    Minor cleanup

--- functions.php
+++ functions.php
@@ -8,56 +8,56 @@
  * @package custom_theme
  */
 
-require  get_template_directory()  . '/vendor/autoload.php';
+require_once get_template_directory()  . '/vendor/autoload.php';
 
 /**
  * Add app constants.
  */
-require  get_template_directory()  . '/inc/constants.php';
+require_once get_template_directory()  . '/inc/constants.php';
 /**
  * Add initial setup.
  */
-require get_template_directory() . '/inc/init.php';
+require_once get_template_directory() . '/inc/init.php';
 
 /**
  * Add theme setup.
  */
-require get_template_directory() . '/inc/theme-setup.php';
+require_once get_template_directory() . '/inc/theme-setup.php';
 
 
 /**
  * Add custom widgets.
  */
-require get_template_directory() . '/inc/widgets.php';
+require_once get_template_directory() . '/inc/widgets.php';
 
 /**
  * Enqueue custom scripts.
  */
-require get_template_directory() . '/inc/load-scripts.php';
+require_once get_template_directory() . '/inc/load-scripts.php';
 
 /**
  * Custom template tags for this theme.
  */
-require get_template_directory() . '/inc/template-tags.php';
+require_once get_template_directory() . '/inc/template-tags.php';
 
 /**
  * Functions which enhance the theme by hooking into WordPress.
  */
-require get_template_directory() . '/inc/template-functions.php';
+require_once get_template_directory() . '/inc/template-functions.php';
 
 /**
  * Customizer additions.
  */
-require get_template_directory() . '/inc/customizer.php';
+require_once get_template_directory() . '/inc/customizer.php';
 
 /**
  * Helper functions.
  */
-require get_template_directory() . '/inc/helpers.php';
+require_once get_template_directory() . '/inc/helpers.php';
 
 /**
  * Load Jetpack compatibility file.
  */
 if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
+	require_once get_template_directory() . '/inc/jetpack.php';
 }
