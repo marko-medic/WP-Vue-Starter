45dd7d7c26497b05645ded1e834b4adb1c60c6d2
Markorf <marko.medic59@gmail.com>
2021-09-01 13:25:00 +0200

    Lint & add more initial files

--- assets/scripts/app.ts
+++ assets/scripts/app.ts
@@ -2,13 +2,6 @@ import { init as navInit } from './shared/sections/nav';
 import { lazyLoadMapper } from './shared/helpers/misc-helpers';
 import { loadVueComponents } from './vue/main';
 
-declare global {
-  interface Window {
-    wp_data: any;
-    jQuery: JQuery;
-  }
-}
-
 loadVueComponents();
 
 console.log('Currently running on: ', window.wp_data.env?.MODE || 'Production');
---
+++ assets/scripts/defs/global.d.ts
@@ -0,0 +1,4 @@
+declare interface Window {
+  wp_data: any;
+  jQuery: JQuery;
+}
